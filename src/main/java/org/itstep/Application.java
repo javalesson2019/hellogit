733ae965a845d3687b933007f107b40572706c7c
mshaptala <mshaptala@gmail.com>
2020-07-09 21:37:58 +0300

    Added empty java project

--- src/main/java/org/itstep/Application.java
+++ src/main/java/org/itstep/Application.java
@@ -2,6 +2,6 @@ package org.itstep;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello github");
+        System.out.println("Привет Мир");
     }
 }
